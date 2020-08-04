2b3aa341239b5b3f8a6a86e294215902770aacf8
Claire Wong <65694059+clairewong827@users.noreply.github.com>
2020-08-04 19:13:14 -0400

    Delete test.sql

--- test.sql
+++
@@ -1,8 +0,0 @@
--- Reporting KPIs: revenue, no.of movie rentals, no.of active customers
--- Calculate the revenue, no. of movie rentals & no. of active customers in 2018
-SELECT SUM(m.renting_price),  COUNT(*),  COUNT(DISTINCT r.customer_id) 
-FROM renting AS r 
-LEFT JOIN movies AS m 
-ON r.movie_id = m.movie_id 
--- Only look at movie rentals in 2018 
-WHERE date_renting BETWEEN '2018-01-01' AND '2018-12-31' ; 
